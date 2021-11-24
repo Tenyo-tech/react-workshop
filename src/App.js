e59ec2cc157898f87645974af0d36f2eb3391a58
Tenyo Hristov <tenyowork90@gmail.com>
2021-11-24 20:19:35 +0200

    Add simple page router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
+import { useState } from 'react';
+
 import Header from './components/Header.js';
 import WelcomeWorld from './components/WelcomeWorld.js';
-import Login from './components/Login.js';
-import Register from './components/Register.js';
 import CreateGame from './components/CreateGame.js';
-import EditGame from './components/EditGame.js';
-import GameDetails from './components/GameDetails.js';
 import GameCatalog from './components/GameCatalog.js';
 
 
 function App() {
+  const [page, setPage] = useState('/home')
+
   const routes = {
-    '/home': WelcomeWorld,
-    '/games': GameCatalog,
-    '/create-game': CreateGame,
+    '/home': <WelcomeWorld />,
+    '/games': <GameCatalog/>,
+    '/create-game': <CreateGame/>,
   };
 
+  const navigationChangeHandler = (path) => {
+    
+    setPage(path);
+    console.log(`${page} change`);
+  }
   return (
     <div id="box">
-      <Header />
+      <Header 
+      navigationChangeHandler = {navigationChangeHandler}
+      />
         
 
         <main id="main-content">
-        <WelcomeWorld />
+         { routes[page] || <h2>No Page Found!</h2>}
         </main>
 
-        <Login/>
+        {/* <Login/>
 
         <Register/>
 
@@ -34,7 +41,7 @@ function App() {
 
         <GameDetails/>
 
-        <GameCatalog/>
+        <GameCatalog/> */}
 
     </div>
   );
