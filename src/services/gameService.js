bf2af0491857b0ad317442ec7d115aa3e506a4a1
Tenyo Hristov <tenyowork90@gmail.com>
2021-11-27 14:44:01 +0200

    add game details

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,4 +1,11 @@
+const baseUrl = 'http://localhost:3030/data/games';
+
 export function getall() {
-    return fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
+    return fetch(`${baseUrl}?sortBy=_createdOn%20desc`)
+        .then(res => res.json());
+}
+
+export function getOne(id) {
+    return fetch(`${baseUrl}/${id}`)
         .then(res => res.json());
 }
